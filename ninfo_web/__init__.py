49e05b5d7a9eaeb8676fd4999fb19f7fbda43c10
Justin Azoff <justin@bouncybouncy.net>
2011-07-25 15:59:02 -0400

    add json route. implement threadlocal stuffs

--- ninfo_web/__init__.py
+++ ninfo_web/__init__.py
@@ -20,11 +20,25 @@ def auth(fn, *a, **kw):
 #bottle.debug(True)
 app = Bottle()
 
-P = Ninfo()
+import logging
+log = logging.getLogger("ninfo-web")
+
+import threading
+local_data = threading.local()
+
+def get_info_object():
+    if hasattr(local_data, 'info'):
+        return local_data.info
+
+    info = Ninfo()
+    log.debug("Creating new info object for %s" % threading.currentThread())
+    local_data.info = info
+    return info
 
 @app.route("/info/text/:plugin/:arg")
 @auth
 def info_text(plugin, arg):
+    P = get_info_object()
     if plugin not in P.plugins:
         abort(404)
     timeout = P.get_plugin(plugin).cache_timeout
@@ -35,17 +49,29 @@ def info_text(plugin, arg):
 @app.route("/info/html/:plugin/:arg")
 @auth
 def info_html(plugin, arg):
+    P = get_info_object()
     if plugin not in P.plugins:
         abort(404)
     timeout = P.get_plugin(plugin).cache_timeout
     response.headers['Cache-Control'] = 'max-age=%d' %  timeout
     return P.get_info_html(plugin, arg)
 
+@app.route("/info/json/:plugin/:arg")
+@auth
+def info_json(plugin, arg):
+    P = get_info_object()
+    if plugin not in P.plugins:
+        abort(404)
+    timeout = P.get_plugin(plugin).cache_timeout
+    response.headers['Cache-Control'] = 'max-age=%d' %  timeout
+    return P.get_info_json(plugin, arg)
+
 @app.route("/")
 @app.route("/info")
 @auth
 @view("info.mako")
 def info():
+    P = get_info_object()
     arg = request.GET.get("arg",'')
     plugins = sorted(P.plugin_classes, key=lambda x:x.name)
     return {"arg": arg, "plugins": plugins}
@@ -53,6 +79,7 @@ def info():
 @app.route("/aboutplugin/:plugin")
 @view("aboutplugin.mako")
 def aboutplugin(plugin):
+    P = get_info_object()
     p = P.get_plugin(plugin)
     return {"p": p}
 
@@ -61,7 +88,6 @@ import make_auth
 app = make_auth.make_auth_app(app)
 
 def main():
-    import logging
     logging.basicConfig(level=logging.DEBUG)
     run(app, server='auto', port=8000)
 
